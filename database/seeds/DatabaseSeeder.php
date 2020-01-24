ad9f6aa0ae704f5fea150174dcc0f0d6253ecd7a
aldiardiansyah123 <aldi03763@gmail.com>
2020-01-24 11:10:05 +0700

    file baru

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,6 +12,6 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
-        $this->call(siswaSeeder::class);
+        $this->call(barangSeeder::class);
     }
 }
